cbb0d45058e84dccd97ab9a4790684cbbd6f58d4
TsuMakoto <tsukamoto@fusic.co.jp>
2021-02-07 14:08:28 +0900

    Include List show

--- src/ToDoCli.jl
+++ src/ToDoCli.jl
@@ -1,26 +1,11 @@
 module Task
 
-using Comonicon
+include("./ToDoCli/lists.jl")
 
-@cast mycmd1(arg; option="Sam") = println("cmd1: arg=", arg, "option=", option)
-@cast mycmd2(arg; option="Sam") = println("cmd2: arg=", arg, "option=", option)
-
-"""
-a module
-"""
-module Cmd3
+using .List
 
 using Comonicon
 
-@cast mycmd4(arg) = println("cmd4: arg=", arg)
-
-end # module
-
-@cast Cmd3
-
-"""
-my demo Comonicon CLI project.
-"""
 @main
 
 end # module
