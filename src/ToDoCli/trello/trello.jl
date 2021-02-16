f1369974ebb99af2db01c3d2bda08295f46cc447
TsuMakoto <tsukamoto@fusic.co.jp>
2021-02-16 20:26:12 +0900

    少しコードを整理

--- src/ToDoCli/trello/trello.jl
+++ src/ToDoCli/trello/trello.jl
@@ -5,42 +5,20 @@ using JSON
 
 baseurl = "https://api.trello.com"
 
-boards() = HTTP.
-             get(merge_auth("$(baseurl)/1/members/me/boards")).
-             body |>
-             String |>
-             JSON.parse
+get(url::String) = HTTP.get(url).body |> String |> JSON.parse
 
-lists(boardid::String) = HTTP.
-                          get(merge_auth("$(baseurl)/1/boards/$(boardid)/lists")).
-                          body |>
-                          String |>
-                          JSON.parse
+merge_auth(url::String) = "$(url)?key=$(ENV["TRELLO_KEY"])&token=$(ENV["TRELLO_TOKEN"])"
 
-cards(listid::String) =  HTTP.
-                          get(merge_auth("$(baseurl)/1/lists/$(listid)/cards")).
-                          body |>
-                          String |>
-                          JSON.parse
+boards() = get(merge_auth("$(baseurl)/1/members/me/boards"))
 
-attachments(cardid::String) = HTTP.
-                                get(merge_auth("$(baseurl)/1/cards/$(cardid)/attachments")).
-                                body |>
-                                String |>
-                                JSON.parse
+lists(boardid::String) = get(merge_auth("$(baseurl)/1/boards/$(boardid)/lists"))
 
-checkitems(cardid::String) = HTTP.
-                                get(merge_auth("$(baseurl)/1/cards/$(cardid)/checkitems")).
-                                body |>
-                                String |>
-                                JSON.parse
+cards(listid::String) =  get(merge_auth("$(baseurl)/1/lists/$(listid)/cards"))
 
-checklists(cardid::String) = HTTP.
-                                get(merge_auth("$(baseurl)/1/cards/$(cardid)/checklists")).
-                                body |>
-                                String |>
-                                JSON.parse
+attachments(cardid::String) = get(merge_auth("$(baseurl)/1/cards/$(cardid)/attachments"))
 
-merge_auth(url) = "$(url)?key=$(ENV["TRELLO_KEY"])&token=$(ENV["TRELLO_TOKEN"])"
+checkitems(cardid::String) = get(merge_auth("$(baseurl)/1/cards/$(cardid)/checkitems"))
+
+checklists(cardid::String) = get(merge_auth("$(baseurl)/1/cards/$(cardid)/checklists"))
 
 end
