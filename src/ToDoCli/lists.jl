536947da6f43f92279ec94826897d44942696dc1
TsuMakoto <tsukamoto@fusic.co.jp>
2021-02-16 20:58:34 +0900

    create cardのフロー作成まで

--- src/ToDoCli/lists.jl
+++ src/ToDoCli/lists.jl
@@ -10,6 +10,8 @@ using Markdown
 
 info = Dict()
 
+API = Trello
+
 set_info(info::Dict) = global info = info
 
 """
@@ -18,19 +20,35 @@ Display task list.
 @cast function show(board_name::String; card::Bool=false)
   switch(board_name)
 
-  ret = DisplayTask.show_list(Trello, info[:boardid]; is_show_card=card)
+  ret = DisplayTask.show_list(API, info[:boardid]; is_show_card=card)
 
-  Markdown.parse(ret) |> display
+  render(ret)
+end
+
+@cast function add(board_name::String, list::String)
+  switch(board_name)
+
+  list = API.find_list_by_name(list)
+
+  ret = API.create_card(list.id)
+
+  disp = DisplayTask.create_card(ret)
+
+  render(disp)
 end
 
 function switch(name::String)
-  boards = Trello.boards()
+  boards = API.boards()
 
   for board in boards
     if (board["name"] == name)
       info[:boardid] = board["id"]
     end
   end
+end
+
+function render(disp)
+  Markdown.parse(disp) |> display
 end
 
 end
--- src/ToDoCli/trello/trello.jl
+++ src/ToDoCli/trello/trello.jl
@@ -7,18 +7,24 @@ baseurl = "https://api.trello.com"
 
 get(url::String) = HTTP.get(url).body |> String |> JSON.parse
 
+post(url::String) = HTTP.post(url)
+
 merge_auth(url::String) = "$(url)?key=$(ENV["TRELLO_KEY"])&token=$(ENV["TRELLO_TOKEN"])"
 
+# ボード
 boards() = get(merge_auth("$(baseurl)/1/members/me/boards"))
 
 lists(boardid::String) = get(merge_auth("$(baseurl)/1/boards/$(boardid)/lists"))
 
 cards(listid::String) =  get(merge_auth("$(baseurl)/1/lists/$(listid)/cards"))
 
+create_card(idlist::String) = post(merge_auth("https://api.trello.com/1/cards") + "&idList=idList")
+
 attachments(cardid::String) = get(merge_auth("$(baseurl)/1/cards/$(cardid)/attachments"))
 
 checkitems(cardid::String) = get(merge_auth("$(baseurl)/1/cards/$(cardid)/checkitems"))
 
 checklists(cardid::String) = get(merge_auth("$(baseurl)/1/cards/$(cardid)/checklists"))
+
 
 end
